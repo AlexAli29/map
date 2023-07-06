18836df84bb922dbbdac6429e7f8fb2398689c7e
Alexander Svetlichny <alexsvetlichny29@gmail.com>
2023-07-06 14:21:18 +0300

    cors setup

--- map-api/src/main.ts
+++ map-api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CLIENT_HOST,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
